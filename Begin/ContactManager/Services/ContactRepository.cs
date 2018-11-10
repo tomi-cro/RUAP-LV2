decbb52437b5f8f387773c088555b02ed727859f
Tomislav Cajbert <tcajbert@gmail.com>
2018-11-10 13:17:33 +0100

    Zad 2-Task 2

--- Begin/ContactManager/Services/ContactRepository.cs
+++ Begin/ContactManager/Services/ContactRepository.cs
@@ -4,25 +4,76 @@ using System.Linq;
 using System.Web;
 using ContactManager.Models;
 
+
 namespace ContactManager.Services
 {
     public class ContactRepository
     {
-        public Contact[] GetAllContacts()
+        private const string CacheKey = "ContactStore";
+        public ContactRepository()
         {
-            return new Contact[]
+            var ctx = HttpContext.Current;
+
+            if (ctx != null)
             {
+                if (ctx.Cache[CacheKey] == null)
+                {
+                    var contacts = new Contact[]
+                    {
                 new Contact
                 {
-                    Id = 1,
-                    Name = "Glenn Block"
+                    Id = 1, Name = "Glenn Block"
                 },
                 new Contact
                 {
-                    Id = 2,
-                    Name = "Dan Roth"
+                    Id = 2, Name = "Dan Roth"
                 }
-            };
+                    };
+
+                    ctx.Cache[CacheKey] = contacts;
+                }
+            }
+        }
+        public Contact[] GetAllContacts()
+        {
+            var ctx = HttpContext.Current;
+
+            if (ctx != null)
+            {
+                return (Contact[])ctx.Cache[CacheKey];
+            }
+
+            return new Contact[]
+                {
+            new Contact
+            {
+                Id = 0,
+                Name = "Placeholder"
+            }
+                };
+        }
+        public bool SaveContact(Contact contact)
+        {
+            var ctx = HttpContext.Current;
+
+            if (ctx != null)
+            {
+                try
+                {
+                    var currentData = ((Contact[])ctx.Cache[CacheKey]).ToList();
+                    currentData.Add(contact);
+                    ctx.Cache[CacheKey] = currentData.ToArray();
+
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                    return false;
+                }
+            }
+
+            return false;
         }
     }
 }
